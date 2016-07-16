9956a69645958bac6408b58a1ae5d416cca4701a
wasimbeniwale <wasimb4@gmail.com>
2016-07-16 09:08:49 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('www/index.html', {
+    'outerBounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
+});
